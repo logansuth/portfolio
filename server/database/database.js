8573f5d4a830bf5a3070c9ea7a9650485278a0fb
Logan Sutherland <loganrsutherland@gmail.com>
2021-04-27 15:35:38 -0400

    chore: change logging to false

--- server/database/database.js
+++ server/database/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const db = new Sequelize(
   process.env.DATABASE_URL || 'postgres://localhost:5432/boilerplate',
   {
-    logging: true,
+    logging: false,
   }
 );
 
