0ffea999368193977eaf3a9fb8ba415783a8eb61
Logan Sutherland <loganrsutherland@gmail.com>
2021-04-29 16:00:50 -0500

    feat: connect header component and add image

---
+++ app/components/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Header = (props) => {
+  return <img src="images/circle-cropped.png" />;
+};
--- app/main.js
+++ app/main.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Header } from './components';
 
 import store from './store';
 import '../public/style.css';
 
 render(
   <Provider store={store}>
-    <div>Hello, World!</div>
+    <Header />
   </Provider>,
   document.getElementById('app')
 );
